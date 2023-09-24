9bd3e74ff4e202728f6aafda194b4aa0622deb28
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-09-24 12:34:25 +0100

    update syntax. Add Main Menu. Google API is fully connected

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-# imported with thanks to Code Institute
+# Google API imported with thanks to Code Institute Tutorial 'Love Sandwiches' by Anna Greaves.
 
-import json
+import json 
 import gspread
-from oauth2client.service_account import Credentials
+from google.oauth2.service_account import Credentials
 
 from pprint import pprint
 
@@ -17,7 +17,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE) #creds.with_scopes is a method that take
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS) # gspread.authorize is a method that takes in the SCOPED_CREDS variable. This variable is the credentials we created to access the API's.
 SHEET = GSPREAD_CLIENT.open('Steam_Test') # name of the spreadsheet
 
-total = SHEET.worksheet('Total') # name of the worksheet
-data = total.get_all_values() # get all values from the worksheet
+def mainMenu():
+    """
+    The main menu of the game
+    """
+    print("Welcome to Steam Test!")
+    print("Please select an option from the menu below:")
+    print("1 - Begin Test")
+    print("2 - View Leaderboard")
+    print("3 - How to Play")
+    print("4 - About STEAM")
+    print("5 - Exit")
 
-print (data)
+def main():
+    """
+    run all program functions, starting with the main menu.
+    """
+    mainMenu()
+    
+
+main()
