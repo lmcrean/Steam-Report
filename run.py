e9a68dbe5b54a3ccf961cbaf0d0803ddd6370c7a
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-09-24 17:04:37 +0100

    Add Quiz functions for getting answer, shuffling choices, receiving input

--- run.py
+++ run.py
@@ -69,10 +69,38 @@ def getTriviaQuestions(amount: int, category: int) -> list:
     response_json = response.json()
     return response_json["results"]
 
-def printAnswerChoice
+def shuffleAnswerChoices(choices: list) -> list:
+    random.shuffle(choices)
+    return choices
 
-def getUserAnswer
+def printAnswerChoices(choices: list) -> None:
+    for choice_index, choice in enumerate(choices):
+        print(f"{choice_index+1}. {html.unescape(choice)}")
 
-def playScienceTrivia
+def getUserAnswer() -> int:
+    while True:
+        user_choice = int(input("Enter the number of your choice: "))
+        if user_choice in range(1, 5): # 1,2,3 or 4
+            return user_choice - 1
+        else:
+            print("Invalid input. Enter a number between 1 and 4")
+
+def playQuiz (amount: int, category: int) -> None:
+    question_pool = get_question_pool(amount, category)
+    for question in question_pool:
+        question_text = html.unescape(question["question"])
+        print(question_text)
+        choices = question ["incorrect_answers"]
+        choices.extend([question["correct_answer"]])
+        shuffled_choices = shuffle_choices(choices)
+        print_choices(shuffled_choices)
+        user_choice_index = get_user_choice()
+        user_choice_text = shuffled_choices[user_choice_index]
+        correct_choice_text = html.unescape(question["correct_answer"])
+
+        if user_choice_text == correct_choice_text
+            print(f"Correct! You answered: {correct_choice_text}\n")
+        else:
+            print(f"Incorrect. The correct answer is {correct_choice_text}\n")
 
 main()
