5fafc03e8b65ec41ab1332f3fbc2fa24c3d2c33f
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-10-05 11:14:39 +0100

    Load personality statements in .py file

---
+++ personality.py
@@ -0,0 +1,5 @@
+import json
+
+with open("personality_statements.json", "r") as file: # Load the questions from the JSON file. R = read.
+    quiz_data = json.load(file) #“Json.load in Python.” GeeksforGeeks, GeeksforGeeks, 12 Mar. 2020, www.geeksforgeeks.org/json-load-in-python/. Accessed 5 Oct. 2023.
+
