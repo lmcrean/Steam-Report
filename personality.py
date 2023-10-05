9e4f339ce6b8aa776468f306f7b63e95db84be57
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-10-05 11:44:45 +0100

    use while loop for invalid response to question

--- personality.py
+++ personality.py
@@ -15,17 +15,21 @@ def ask_question(question_index): #question_index is a parameter. “Python Func
     question = quiz_data["questions"][question_index]
     print(f"Question {question_index + 1}:")
     print(question["statement"])
-    response = input("Please enter a number from 1 to 9 (1 = Strongly Disagree, 5=Neutral, 9 = Strongly Agree): ")
 
-    try:
-        response = int(response) # Convert the user's response to an integer
-        if 1 <= response <= 9:
-            user_answers.append(response) # Add the response to the user's answers
-            trait_scores[question["trait"]] += response  # Add the response to the corresponding trait score
-        else:
+    while True:
+        response = input("Please enter a number from 1 to 9 (1 = Strongly Disagree, 5 = Neutral, 9 = Strongly Agree): ")
+
+        try:
+            response = int(response) # Convert the user's response to an integer
+            if 1 <= response <= 9:
+                user_answers.append(response) # Add the response to the user's answers
+                trait_scores[question["trait"]] += response  # Add the response to the corresponding trait score
+                break  # Exit the loop when a valid response is provided
+            else:
+                print("Invalid response. Please enter a number between 1 and 9.")
+        except ValueError:
             print("Invalid response. Please enter a number between 1 and 9.")
-    except ValueError:
-        print("Invalid response. Please enter a number between 1 and 9.")
+
 
 # Loop through the questions and ask them one by one
 while question_index < len(quiz_data["questions"]): #“Python Len() Function.” W3schools.com, 2023, www.w3schools.com/python/ref_func_len.asp. Accessed 5 Oct. 2023. Len() function returns the number of items in an object.
