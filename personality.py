b77cec3f366fd8a7a053f52d99f01d67f0033caa
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-10-05 11:17:46 +0100

    initialize variables

--- personality.py
+++ personality.py
@@ -3,3 +3,6 @@ import json
 with open("personality_statements.json", "r") as file: # Load the questions from the JSON file. R = read.
     quiz_data = json.load(file) #“Json.load in Python.” GeeksforGeeks, GeeksforGeeks, 12 Mar. 2020, www.geeksforgeeks.org/json-load-in-python/. Accessed 5 Oct. 2023.
 
+# Initialize variables including trait scores.
+user_answers = [] 
+trait_scores = {"Openness": 0, "Conscientiousness": 0, "Extraversion": 0, "Agreeableness": 0, "Neuroticism": 0}
