37e4615eb99c28831bf7cf232abd2b9170708a06
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-10-05 11:26:04 +0100

    Add while loop for personality questions

--- personality.py
+++ personality.py
@@ -6,8 +6,9 @@ with open("personality_statements.json", "r") as file: # Load the questions from
 # Initialize variables including trait scores.
 user_answers = [] 
 trait_scores = {"Openness": 0, "Conscientiousness": 0, "Extraversion": 0, "Agreeableness": 0, "Neuroticism": 0}
+question_index = 0
 
-def ask_question(question_index):
+def ask_question(question_index): #question_index is a parameter. “Python Function Arguments.” W3schools.com, 2023, www.w3schools.com/python/gloss_python_function_arguments.asp#:~:text=A%20parameter%20is%20the%20variable,function%20when%20it%20is%20called. Accessed 5 Oct. 2023.
     question = quiz_data["questions"][question_index]
     print(f"Question {question_index + 1}:")
     print(question["statement"])
@@ -22,3 +23,8 @@ def ask_question(question_index):
             print("Invalid response. Please enter a number between 1 and 9.")
     except ValueError:
         print("Invalid response. Please enter a number between 1 and 9.")
+
+# Loop through the questions and ask them one by one
+while question_index < len(quiz_data["questions"]): #“Python Len() Function.” W3schools.com, 2023, www.w3schools.com/python/ref_func_len.asp. Accessed 5 Oct. 2023. Len() function returns the number of items in an object.
+    ask_question(question_index)
+    question_index += 1
