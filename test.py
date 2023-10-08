2e2c81a1dd578bbc5338096e2066c773db4dc321
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-10-08 16:59:57 +0100

    define function to calculate Highest Score from data pool with dummy username. Correctly prints with VSCode testing

--- test.py
+++ test.py
@@ -73,110 +73,95 @@ class SubjectScore:
 
 subject_scores = SubjectScore(0,0,0,0,0,0)
 
+username_str = "Betty56"
 
-def viewLeaderboard():
-    print("You have chosen to view the leaderboard.")
-    print("This is a work in progress.")
-    print("1 - submit high score")
-    print("2 - view leaderboard")
-    choice = int(input("Please enter your choice: "))
-    if choice == 1:
-        leaderboardMain(subject_scores)
-    elif choice == 2:
-        get_high_score_leaderboard()
-    else:
-        print("Please enter a number between 1 and 2.")
-
-def get_user_data(subject_scores: SubjectScore) -> None:
-    """
-    Gets the score and username from user and returns the username.
-    """
-
-    while True: # True
-        
-        username_str = input("Enter your username here: ") # ask the user for their username
-
-        #place high score data into user_data_string variable
-        user_data_string = f"{username_str},{subject_scores.scoreTotal},{subject_scores.scoreScience},{subject_scores.scoreTechnology},{subject_scores.scoreEnglish},{subject_scores.scoreArt},{subject_scores.scoreMath}" 
-
-        user_data = user_data_string.split(",") # split the user_data string into a list of strings at each comma. This will create a list of strings. The first item in the list will be the username, and the rest of the items will be the scores.
-
-        print(f"User Data: {user_data}\n") # print the user_data list
-
-        break # break out of the while loop if data is valid
-    print("Data is ready to be uploaded to Google Sheets.\n")
-    return user_data # return the user_data list
-
-
-def update_worksheet(data, worksheet):
-    """
-    Receives a list of strings and integers to be inserted into a worksheet.
-    Update the worksheet with the data provided.
-    """
-    print(f"Updating {worksheet} worksheet...\n")
-    worksheet_to_update = SHEET.worksheet(worksheet) # access the relevant worksheet
-    print(f"Data to be inserted: {data}\n")
-    worksheet_to_update.append_row(data) # append the test user values provided as a new row at the bottom of the relevant worksheet
-    print(f"{worksheet} worksheet updated successfully.\n")
-
-def get_high_score_leaderboard():
+def generate_comparison_data_main():
     """
     Collects columns of data from score worksheet, collecting the last 5 entries for each sandwich and returns the data as a list of lists.
 
     “Prettytable.” PyPI, 11 Sept. 2023, pypi.org/project/prettytable/. Accessed 1 Oct. 2023.
     """
+    calculateHighestSTEAMScore()
+    calculateHighestOCEANScore()
 
-    from prettytable import PrettyTable
-
+def calculateHighestSTEAMScore():
     worksheet = SHEET.worksheet('score')  # Access worksheet
     data = worksheet.get_all_values()  # Read data
-    table, table2 = PrettyTable(), PrettyTable()  # Create PrettyTable objects
-    table.field_names = table2.field_names = data[0]  # Set field names
+    table = PrettyTable()  # Create PrettyTable object
+    table.field_names = data[0]  # Set field names
     all_user_info = []
     for row in data[1:]:  # Populate table and collect user info
         table.add_row(row)
         userinfo = dict(zip(table.field_names, row))
         all_user_info.append(userinfo)
-    print("Original Table")
-    print(table)  # Display original table
-    # Sort and display table by Science Score
-    sortedByScience = sorted(all_user_info, key=lambda d: int(d['S']), reverse=True)  
-    table_sorted_by_science = PrettyTable()
-    table_sorted_by_science.field_names = data[0]
-    for row_dict in sortedByScience:
-        row = [row_dict[field] for field in table_sorted_by_science.field_names]
-        table_sorted_by_science.add_row(row)
-    print("Sorted by Science Score")
-    print(table_sorted_by_science)
-
-
+    localuser_data = next((item for item in all_user_info if item["Username"] == username_str), None)    # Filter out the data for username
+    if localuser_data:
+        # Extract STEAM scores and find the highest category
+        steam_categories = ["S", "T", "E", "A", "M"]
+        highest_score = -1
+        highest_category = ""
+        for category in steam_categories:
+            score = int(localuser_data[category])
+            if score > highest_score:
+                highest_score = score
+                highest_category = category
+            if highest_category == "S":
+                highest_category = "Science"
+            elif highest_category == "T":
+                highest_category = "Technology"
+            elif highest_category == "E":
+                highest_category = "English"
+            elif highest_category == "A":
+                highest_category = "Art"
+            elif highest_category == "M":
+                highest_category = "Math"
+        
+        print(f"You scored highest in {highest_category}")
+    else:
+        print(f"Username {username_str} not found")
 
     #“How to Sort a List of Dictionaries by a Value of the Dictionary in Python?” Stack Overflow, 2023, stackoverflow.com/questions/72899/how-to-sort-a-list-of-dictionaries-by-a-value-of-the-dictionary-in-python. Accessed 5 Oct. 2023.
     #“Sort a List of Objects in Python | FavTutor.” FavTutor, 2022, favtutor.com/blogs/sort-list-of-objects-python. Accessed 5 Oct. 2023.
 
+def calculateHighestOCEANScore():
+    worksheet = SHEET.worksheet('personality')  # Access 'personality' worksheet
+    data = worksheet.get_all_values()  # Read data
+    table = PrettyTable()  # Create PrettyTable object
+    table.field_names = data[0]  # Set field names
+    all_user_info = []
 
+    for row in data[1:]:  # Populate table and collect user info
+        table.add_row(row)
+        userinfo = dict(zip(table.field_names, row))
+        all_user_info.append(userinfo)
 
-    # table.sortby = "Score" # Sort the table by the Total column, in ascending order
-    # table.reversesort = True # Reverse the order of the sort, so it's descending
+    # Filter out the data for the specific username
+    localuser_data = next((item for item in all_user_info if item["Username"] == username_str), None)
 
-    # print(table) # Print the PrettyTable
+    if localuser_data:
+        # Extract OCEAN scores and find the highest category
+        ocean_categories = ["O", "C", "E", "A", "N"]
+        highest_score = -1
+        highest_category = ""
 
-    #TODO: 1 insert dummy data into the Rank column from a string
+        for category in ocean_categories:
+            score = int(localuser_data[category])
+            if score > highest_score:
+                highest_score = score
+                highest_category = category
+            if highest_category == "O":
+                highest_category = "Openness"
+            elif highest_category == "C":
+                highest_category = "Conscientiousness"
+            elif highest_category == "E":
+                highest_category = "Extraversion"
+            elif highest_category == "A":
+                highest_category = "Agreeableness"
+            elif highest_category == "N":
+                highest_category = "Neuroticism"
 
-    return 
+        print(f"You scored highest in {highest_category}")
+    else:
+        print(f"Username {username_str} not found")
 
-def leaderboardMain(subject_scores): #leaderboardmain() uses the subject_scores variable
-    """
-    Run all program functions
-    """
-    print("Welcome to the leaderboard!")
-    # subject_scores = SubjectScore(0,0,0,0,0,0) # create a SubjectScore object and store it in a variable called subject_scores
-    data = get_user_data(subject_scores)  # call the get_user_data function and store the returned data in a variable called data
-    print(f"Data provided: {data}\n")
-    user_data = data  # convert the data provided by the user into integers. num is a variable that represents each item in the list data. 
-    print(f"User Data:{user_data}") 
-    update_worksheet(user_data, "score")  # call the update_worksheet function with user_data as a list
-    get_high_score_leaderboard()  # call the get_high_score_leaderboard function and store the returned data in a variable called score_columns
-
-
-get_high_score_leaderboard()
+generate_comparison_data_main()
